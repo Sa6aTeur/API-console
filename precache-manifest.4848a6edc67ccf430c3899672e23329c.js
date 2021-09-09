9fcebf685c027984d7b9e66e581bd78cb4ae06f6
yuri.lysenko <yuri.lysenko@bonrepublic.com>
2021-09-09 13:19:09 +0300

    Updates

---
+++ precache-manifest.4848a6edc67ccf430c3899672e23329c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7173d45172220e8fe094c6ae80295d",
+    "url": "/API-console/index.html"
+  },
+  {
+    "revision": "88fcd5f33927a5bbfdc7",
+    "url": "/API-console/static/css/main.f7b9ba9a.chunk.css"
+  },
+  {
+    "revision": "aededc595e6e7b3dd2fc",
+    "url": "/API-console/static/js/2.9cf398a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/API-console/static/js/2.9cf398a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88fcd5f33927a5bbfdc7",
+    "url": "/API-console/static/js/main.ea059c24.chunk.js"
+  },
+  {
+    "revision": "352213b16d8f1588ae6d",
+    "url": "/API-console/static/js/runtime-main.1fbbd6fa.js"
+  }
+]);
