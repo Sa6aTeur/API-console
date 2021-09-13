cf9a73f81280b6528cf8411ca0a38caaec740faf
yuri.lysenko <yuri.lysenko@bonrepublic.com>
2021-09-13 22:00:26 +0300

    Add auth check in console page and fix cross svg props

--- src/components/ui/Cross.js
+++ src/components/ui/Cross.js
@@ -17,8 +17,8 @@ function Cross(props) {
     return (
         <div className={styles.root} {...props}>  
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="var(--cross-color)" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 1L19 19" stroke="var(--cross-color)" stroke-width="2" stroke-linecap="round"/>
-                    <path d="M19 1L1 19" stroke="var(--cross-color)" stroke-width="2" stroke-linecap="round"/>
+                    <path d="M1 1L19 19" stroke="var(--cross-color)" strokeWidth="2" strokeLinecap="round"/>
+                    <path d="M19 1L1 19" stroke="var(--cross-color)" strokeWidth="2" strokeLinecap="round"/>
                 </svg>
             </div> 
     )
--- src/containers/ConsolePage.js
+++ src/containers/ConsolePage.js
@@ -1,5 +1,7 @@
+import React, {useEffect} from 'react'
+import { useHistory } from 'react-router'
 import { makeStyles } from '@material-ui/core'
-import React from 'react'
+import { useLoginContext } from 'src/context/LoginContext';
 import Header from 'src/components/console/Header/Header'
 import RequestBlock from 'src/components/console/RequestBlock/RequestBlock'
 import RequestList from 'src/components/console/RequestList/RequestList'
@@ -14,7 +16,21 @@ const useStyles = makeStyles((theme) => ({
 
 function ConsolePage() {
     const styles = useStyles()
-    
+    const history = useHistory()
+    const {loginData} = useLoginContext()
+    const {sessionKey} = loginData
+    const isLoggedIn = !!sessionKey?.length;
+
+    useEffect(() => {
+        if (!isLoggedIn) {   
+          history.push('/');
+        }
+    }, [history, isLoggedIn]);
+
+    if(!isLoggedIn){
+        return <></>
+    }
+
     return (
         <div className={styles.root}>
             <Header />
